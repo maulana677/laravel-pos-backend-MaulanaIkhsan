91ac1bb8af211e611576f7568a89cd0e76750ae7
maulana677 <maull0776@gmail.com>
2023-12-20 15:35:13 +0700

    ubah title

--- resources/views/pages/users/create.blade.php
+++ resources/views/pages/users/create.blade.php
@@ -16,7 +16,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Create Users</h1>
+                <h1>Create Users tes</h1>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="{{ route('home') }}">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="{{ route('user.index') }}">All Users</a></div>
